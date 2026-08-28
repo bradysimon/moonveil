50174be273a5d3907ad0b8e582bdfcda1dac697a
BradySimon <bradytsimon@gmail.com>
2026-08-28 10:39:42 -0400

    Use readme contents as current lib docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,5 @@
-// TODO: Placeholder while awaiting next Iced version release.
+//! A custom theme and design framework for iced.
+//!
+//! This is a placeholder publish while awaiting the next Iced version so that
+//! I can utilize the latest features on the `master` branch. I aim to release
+//! a real version once iced 0.15 is released.
